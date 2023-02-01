9b00524d5ad91a6002b3cc2f84b1fa6acf0aba21
yhdchan <yhdchan1993uk@gmail.com>
2023-02-01 00:36:03 +0000

    Complete URL Shortener Microservice

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
 
 // Basic Configuration
@@ -8,17 +8,58 @@ const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.use("/public", express.static(`${process.cwd()}/public`));
+
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.listen(port, function() {
+const originalUrls = [];
+const shortUrls = [];
+
+app.post("/api/shorturl", (req, res) => {
+  const url = req.body.url;
+  const index = originalUrls.indexOf(url);
+
+  if (!url.includes("https://") && !url.includes("http://")) {
+    return res.json({ error: "invalid url" });
+  }
+
+  if (index < 0) {
+    originalUrls.push(url);
+    shortUrls.push(shortUrls.length + 1);
+
+    return res.json({
+      original_url: url,
+      short_url: shortUrls.length,
+    });
+  }
+
+  return res.json({
+    original_url: url,
+    short_url: shortUrls[index],
+  });
+});
+
+app.get("/api/shorturl/:short_url", (req, res) => {
+  const short_url = parseInt(req.params.short_url);
+  const index = shortUrls.indexOf(short_url);
+
+  if (index < 0) {
+    return res.json({ error: "No such short_url" });
+  }
+
+  res.redirect(originalUrls[index]);
+});
+
+app.listen(port, function (err) {
+  if (err) console.log(err);
   console.log(`Listening on port ${port}`);
 });
